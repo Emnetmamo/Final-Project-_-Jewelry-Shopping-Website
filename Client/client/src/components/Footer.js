8fe50a2b0544b3e52a59062e9e3570bb7f423f84
Emnetmamo <131816255+Emnetmamo@users.noreply.github.com>
2025-05-08 13:52:19 +0300

    Updated code 35

--- Client/client/src/components/Footer.js
+++ Client/client/src/components/Footer.js
@@ -31,7 +31,7 @@ const Footer = () => {
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; 2024 Nomdas Jewelry. All rights reserved.</p>
+        <p>&copy; 2025 Nomdas Jewelry. All rights reserved.</p>
       </div>
     </footer>
   );
