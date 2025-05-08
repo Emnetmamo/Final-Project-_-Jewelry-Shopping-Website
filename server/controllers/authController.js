9ad61dfda96594e175bb5ade8b4c60c870517c4b
Emnetmamo <131816255+Emnetmamo@users.noreply.github.com>
2025-05-08 12:39:53 +0300

    Updated code 26

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey'; 
+const JWT_SECRET = 'supersecretkey'; 
 // User Signup
 exports.signup = async (req, res) => {
     const { username, email, password } = req.body;
